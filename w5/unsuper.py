d82d573f0875bf0b29bf63b0259882dcf99e6e52
recursedd <dde@ncsu.edu>
2018-10-10 13:11:02 -0400

    unsuper w5 completed

--- w5/unsuper.py
+++ w5/unsuper.py
@@ -1,77 +1,74 @@
-from w3.Num import Num
-import math
-from w5.rows import Data
-
-from operator import itemgetter
-
-def dump(a, sep="\t"):
-    for i in a:
-        print(sep.join(str(b) for b in i))
-
-
-def ksort(r,k):
-     r = sorted(r, key=lambda r1:r1[k])
-     return r
-
-
+from w5.dom import Data
+from w3.num import Num
+import util
 def unsuper(data):
     rows = data.rows
-    enough = pow(len(rows),0.5)
+    enough = len(rows) ** 0.55
 
-
-    def band(c,lo,hi):
-        if lo==0:
+    def band(c, lo, hi):
+        if lo == 1:
             return ".." + str(rows[hi][c])
         elif hi == most:
             return str(rows[lo][c]) + ".."
         else:
             return str(rows[lo][c]) + ".." + str(rows[hi][c])
 
-    def argmin(c,lo,hi):
+    def argmin(c, lo, hi):
         cut = None
-        if (hi - lo > 2*enough):
-            l,r = Num(), Num()
-            for i in range(lo,hi+1):
+        if (hi - lo > 2 * enough):
+            l, r = Num(0), Num(0)
+            for i in range(lo, hi + 1):
                 r.numInc(rows[i][c])
             best = r.sd
-            for i in range(lo,hi+1):
+            for i in range(lo, hi + 1):
                 x = rows[i][c]
                 l.numInc(x)
                 r.numDec(x)
                 if l.n >= enough and r.n >= enough:
-                    tmp = l.numXpect(r) * 1.05
+                    tmp = Num.numXpect(l, r) * 1.04
                     if tmp < best:
-                        cut,best = i, tmp
+                        cut, best = i, tmp
         return cut
 
 
-    def cuts(c,lo,hi,pre):
+    def cuts(c, lo, hi, pre):
         txt = pre + str(rows[lo][c]) + ".. " + str(rows[hi][c])
-        cut = argmin(c,lo,hi)
+        cut = argmin(c, lo, hi)
         if cut:
             print(txt)
-            cuts(c,lo,   cut, pre + "|.. ")
-            cuts(c,cut+1, hi, pre + "|.. ")
+            cuts(c, lo, cut, pre + "|.. ")
+            cuts(c, cut + 1, hi, pre + "|.. ")
         else:
-            b= band(c,lo,hi)
-            print(txt +" ("+str(b)+")")
-            for r in range(lo,hi):
-                rows[r][c]=b
+            b = band(c, lo, hi)
+            print(txt + " (" + b + ")")
+            for r in range(lo, hi + 1):
+                rows[r][c] = b
 
 
-    def stop(c,t):
-        for i in range(len(t)-1,-1,-1):
-            if t[i][c] != "?":
-                return i
+    def stop(c, t):
+        for i in range(len(t) - 1, -1, -1):
+            if t[i][c] != "?": return i
         return 0
 
     for c in data.indeps:
-        if data.nums.get(c):
-            rows = ksort(rows,c)
-            #print(rows)
-            most = stop(c,rows)
-            #print("\n-- " + data.name[c] + str(most)+ "----------")
-            cuts(c,0,most,"|.. ")
-    #print(", ".join(data.name.values()))
-    print(", ".join(data.name.values()).replace("$",""))
-dump(rows)
+        if c in data.nums:
+            rows = sorted(rows.values(), key=lambda x:x[c])
+            most = stop(c, rows)
+            print("\n---------- Output ----------")
+            cuts(c, 0, most, "|.. ")
+    print("\n")
+    title = data.name
+    # title = ' '.join(str(title))
+    print(title)
+    for s in rows:
+        print(*s)
+
+
+@util.O.k
+def testUnsuper():
+    # print("WeatherLong.csv")
+    d1 = Data()
+    print ("weatherLong results")
+    d1.readerRows("../w4/weatherLong.csv")
+    unsuper(d1)
+    # unsuper(x)
